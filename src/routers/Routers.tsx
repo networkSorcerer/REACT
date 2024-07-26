ce7df97e663867e9b8a52b58136a590b22ab3e51
networkSocerer <gigattfyhig@gmail.com>
2024-07-26 17:20:57 +0900

    react

--- src/routers/Routers.tsx
+++ src/routers/Routers.tsx
@@ -2,13 +2,23 @@ import { RouteObject, createBrowserRouter } from 'react-router-dom';
 import { Login } from '../pages/Login';
 import { DashBoard } from '../component/layout/DashBoard/DashBoard';
 import { NotFound } from '../component/common/NotFound/NotFound';
-
+import { Notice } from '../pages/Notice'; 
 const routers: RouteObject[] = [
     { path: '*', element: <NotFound /> },
     { path: '/', element: <Login /> },
     {
         path: '/react',
         element: <DashBoard />,
+        children: [
+            {path: 'board', 
+                children : [
+                    {
+                        path : 'notice.do',
+                         element: <Notice />
+                        }
+                    ]
+                }
+        ],
     },
 ];
 
